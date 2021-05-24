44461128ed2cda14042b06e4cfdae1f441df2989
vishalicious213 <vishalicious213@gmail.com>
2021-05-24 14:39:42 -0700

    added OnAdd to App

--- src/App.js
+++ src/App.js
@@ -8,6 +8,15 @@ function App() {
   const { products } = data
   const [cartItems, setCartItems] = useState([])
 
+  const onAdd = (product) => {
+    const exists = cartItems.find(x => x.id === product.id)
+    if(exists) {
+      setCartItems(cartItems.map(x => x.id === product.id ? {...exists, qty: exists.qty + 1} : x))
+    } else {
+    setCartItems([...cartItems, { ...products, qty: 1 }])
+    }
+  }
+
   return (
     <div className="App">
       <Header />
