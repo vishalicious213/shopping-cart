9061d193bf8a530d90aed1f9314c83ff8b31085b
vishalicious213 <vishalicious213@gmail.com>
2021-05-24 14:31:25 -0700

    set up state for cartItems. show empty cart message with conditional rendering

--- src/App.js
+++ src/App.js
@@ -2,16 +2,18 @@ import Header from './components/Header'
 import Gallery from './components/Gallery'
 import Cart from './components/Cart'
 import data from './data'
+import { useState } from 'react'
 
 function App() {
   const { products } = data
+  const [cartItems, setCartItems] = useState([])
 
   return (
     <div className="App">
       <Header />
       <div className='row'>
         <Gallery products={products} />
-        <Cart />
+        <Cart cartItems={cartItems} />
       </div>
     </div>
   );
--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,9 +1,14 @@
 import React from 'react'
 
 export default function Cart(props) {
+    const { cartItems } = props
+
     return (
         <aside className='block col-1'>
             <h2>Cart Items</h2>
+            <div>
+                {cartItems.length === 0 && <div>Cart is empty</div>}
+            </div>
         </aside>
     )
 }
