af9796e2f832a55e5a0eaafbb7fd3c94695da7d6
vishalicious213 <vishalicious213@gmail.com>
2021-05-24 14:53:40 -0700

    added elements to Cart

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,13 +1,25 @@
 import React from 'react'
 
 export default function Cart(props) {
-    const { cartItems, onAdd } = props
+    const { cartItems, onAdd, onRemove } = props
 
     return (
         <aside className='block col-1'>
             <h2>Cart Items</h2>
             <div>
                 {cartItems.length === 0 && <div>Cart is empty</div>}
+                {cartItems.map(item => {
+                    <div key={item.id} className='row'>
+                        <div className='col-2'>{item.name}</div>
+                        <div className='col-2'>
+                            <button onClick={() => onAdd(item)} className='add'>+</button>
+                            <button onClick={() => onRemove(item)} className='remove'>-</button>
+                        </div>
+                        <div className='col-2 text-right'>
+                            {item.qty} x ${item.price.toFixed(2)}
+                        </div>
+                    </div>
+                })}
             </div>
         </aside>
     )
