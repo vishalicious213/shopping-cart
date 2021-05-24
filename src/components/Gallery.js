0715c2b9fab96f71f19f7632b7a6cf8d7b54c34a
vishalicious213 <vishalicious213@gmail.com>
2021-05-24 14:19:53 -0700

    drafted Product, mapped through data and rendered products in Gallery

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,11 +1,16 @@
 import React from 'react'
+import Product from './Product'
 
 export default function Gallery(props) {
+    const { products } = props
+
     return (
         <main className='block col-2'>
             <h2>Products</h2>
             <div className='row'>
-
+                {products.map(product => (
+                    <Product key={product.id} product={product} />
+                ))}
             </div>
         </main>
     )
--- src/data.js
+++ src/data.js
@@ -4,16 +4,19 @@ const data = {
             id: '1',
             name: 'Macbook',
             image: 'https://picsum.photos/id/180/2400/1600',
+            price: '1400'
         },
         {
             id: '2',
             name: 'Old Car',
             image: 'https://picsum.photos/id/111/4400/2656',
+            price: '2400'
         },
         {
             id: '3',
             name: 'W Shoes',
             image: 'https://picsum.photos/id/21/3008/2008',
+            price: '1000'
         },
     ]
 }
